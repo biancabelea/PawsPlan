61fdab7966f282a813183634547d985b4e77ae1e
biancabelea <bianca.belea00@gmail.com>
2023-05-28 01:34:34 +0300

    Add Firebase Auth initial setup

---
+++ paws-plan/src/firebase-config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzZ5k5GSr9IklJl0zDM1yMH8sxfCeQklI",
+    authDomain: "pawsplan-f6073.firebaseapp.com",
+    projectId: "pawsplan-f6073",
+    storageBucket: "pawsplan-f6073.appspot.com",
+    messagingSenderId: "669654788227",
+    appId: "1:669654788227:web:768acd41cd3783261c13b6",
+    measurementId: "G-JF8Q71ZNXF"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
