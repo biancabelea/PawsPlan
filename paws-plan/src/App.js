3ad27b6ac1d8dc9199d03976b3cc7c8b3fda26eb
biancabelea <bianca.belea00@gmail.com>
2023-06-06 00:33:58 +0300

    Added logout button

--- paws-plan/src/App.js
+++ paws-plan/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { auth } from "./firebaseConfig";
 import AddPet from "./components/AddPet";
 import ViewPets from "./components/ViewPets";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 const App = () => {
-    const userId = 'sBoHD7prgza6vYK6ELtgwcGRnh02';
+    const [userId, setUserId] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) {
+                setUserId(user.uid);
+                setLoggedIn(true);
+            } else {
+                setUserId(null);
+                setLoggedIn(false);
+            }
+        });
+        return () => unsubscribe();
+        }, []);
 
     return (
         <div>
             <h1>My Firebase App</h1>
-            <AddPet userId={userId}/>
-            <ViewPets userId={userId}/>
+            {userId ? (
+                <>
+                    <AddPet userId={userId}/>
+                    <ViewPets userId={userId}/>
+                </>
+            ) : (
+                <>
+                    <Register/>
+                    <Login/>
+                </>
+            )}
         </div>
     );
 };
--- paws-plan/src/components/Login.js
+++ paws-plan/src/components/Login.js
@@ -6,45 +6,63 @@ const Login = () => {
 	const [password, setPassword] = useState('');
 	const [loggedEmail, setLoggedEmail] = useState('');
 	const [loggedPassword, setLoggedPassword] = useState('');
+	const [loggedIn, setLoggedIn] = useState(false)
 
 	const handleLogin = async (e) => {
 		e.preventDefault();
 
 		try {
 			await auth.signInWithEmailAndPassword(email, password);
-			console.log('Logged in successfully!');
+			console.log('Log in cu succes!');
 			setLoggedEmail(email);
 			setLoggedPassword(password);
 			setEmail('');
 			setPassword('');
+			setLoggedIn(true);
 		} catch (error) {
-			console.error('Error logging in:', error);
+			console.error('Eroare la log in:', error);
+		}
+	};
+
+	const handleLogout = async () => {
+		try {
+			await auth.signOut();
+			console.log('Log out cu succes!');
+			setLoggedIn(false);
+		} catch (error) {
+			console.error('Eroare la log out:', error);
 		}
 	};
 
 	return (
 		<div>
-			<h2>Login</h2>
-			<form onSubmit={handleLogin}>
-				<input
-					type="email"
-					placeholder="Email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<button type="submit">Log In</button>
-			</form>
-			{loggedEmail && (
-				<p>Logged in as: {loggedEmail}</p>
+			{loggedIn ? (
+				<div>
+					<h2>Bine ai venit, {loggedEmail}!</h2>
+					<button onClick={handleLogout}>Log Out</button>
+				</div>
+			) : (
+				<div>
+					<h2>Login</h2>
+					<form onSubmit={handleLogin}>
+						<input
+							type="email"
+							placeholder="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<input
+							type="password"
+							placeholder="Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<button type="submit">Log In</button>
+					</form>
+				</div>
 			)}
 		</div>
-	);
+	)
 };
 
 export default Login;
