830453d169615c5a29ea24bdd66aef87f9a4ec9c
biancabelea <bianca.belea00@gmail.com>
2023-06-21 02:47:54 +0300

    Homepage with lorem ipsum

--- paws-plan/src/components/Homepage.js
+++ paws-plan/src/components/Homepage.js
@@ -1,10 +1,12 @@
 import React, {useContext, useState} from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Route, Routes } from 'react-router-dom';
 import {UserContext} from "./UserContext";
 import Menu from "./Menu";
 import ViewPets from "./ViewPets";
 // import "../styles/Menu.css";
 import "../styles/Homepage.css";
+import AddPet from "./AddPet";
+import {ReactComponent as Logo} from "../pictures/logo.svg";
 
 const Homepage = () => {
 	const { userId } = useContext(UserContext);
@@ -32,9 +34,18 @@ const Homepage = () => {
 					</div>
 				</div>
 			) : (
-			<div>
-				<button onClick={handleLoginClick}>Go to Login</button>
-				<button onClick={handleRegisterClick}>Go to Register</button>
+			<div className="homepage">
+				<div className="navbar">
+					<div><Logo className="logo-image"/></div>
+					<div className="user-buttons">
+						<button className="user-login-button" onClick={handleLoginClick}>Conectare</button>
+						<button className="user-register-button" onClick={handleRegisterClick}>Înregistrare</button>
+					</div>
+				</div>
+				<div className="motto">
+					<h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+					<h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</h3>
+				</div>
 			</div>
 			)}
 		</div>
